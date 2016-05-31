a9b3f749166cc013c56ee2534630cf71db9c471d
mirsakiyev <mirsakiyev@yahoo.com>
2016-05-31 14:15:16 -0400

    Added Course class

---
+++ COMP123-S2016-Lesson4/Course.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lesson4
+{
+    /**
+     * This is the course class - we are using this as
+     * a custom data typr for our Teacher and Student classes
+     *
+     * @class Course
+     */
+    class Course
+    {
+
+        //----------------------------------------------------------------------------------------------
+        //--------------------------PRIVATE INSTANCE VARIABLES------------------------------------------
+        //----------------------------------------------------------------------------------------------
+        private string _code;
+        private string _description;
+
+
+
+        public string Code
+        {
+            get
+            {
+                return this._code;
+            }
+            set
+            {
+                this._code = value;
+            }
+        }
+
+
+        public string Description
+        {
+            get
+            {
+                return this._description;
+            }
+            set
+            {
+                this._description = value;
+            }
+        }
+
+
+
+        //----------------------------------------------------------------------------------------------
+        //------------------------------------CONSTRUCTORS----------------------------------------------
+        //----------------------------------------------------------------------------------------------
+        /**
+       * <summary>
+       * This constructor method takes 2 parameters: Code and Description
+       * </summary>
+       *
+       * @constructor Course
+       * @param {string} name
+       * @param {string} description
+       */
+
+        public Course(string code, string description)
+        {
+            this.Code = code;
+            this.Description = description;
+        }
+    }
+}
